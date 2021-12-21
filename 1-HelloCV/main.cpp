1323caec48054ab028d30ce7fe4b94ed31a73b92
hanwei <hanwei@xiaomi.com>
2021-12-21 19:47:17 +0800

    First CV project
    Signed-off-by: hanwei <hanwei@xiaomi.com>

--- 1-HelloCV/main.cpp
+++ 1-HelloCV/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cv::Mat mat(400,300,CV_8UC3, cv::Scalar(255,215,0));
+    std::cout << "hello opencv\n";
+    imshow("image",mat);
+    cv::waitKey();
     return 0;
 }
